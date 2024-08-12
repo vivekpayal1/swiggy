1b45958351a5066d15acf850b590d09849abccdc
vivekpayal1 <vivekpayal8954@gmail.com>
2024-08-12 11:26:42 +0530

    fix firebase auth config issue

--- src/utils/firebaseAuth.js
+++ src/utils/firebaseAuth.js
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_AUTH_CONFIG);
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN_KEY,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 export { auth, provider };
